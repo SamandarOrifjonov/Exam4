1084c2189d286fcd9ad6344073e88e3b5ee3fadc
SamandarOrifjonov <135264322+SamandarOrifjonov@users.noreply.github.com>
2024-02-21 15:56:39 +0500

    Task1 is done

--- .idea/src/Task1.java
+++ .idea/src/Task1.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Task1 {
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {Scanner scanner = new Scanner(System.in);
+
+        System.out.print("YYYY-MM-dd: ");
+        String inputDate = scanner.nextLine();
+
+        LocalDate date = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+        System.out.println(date + " day " + dayOfWeek + " of week");
+    }
 }
